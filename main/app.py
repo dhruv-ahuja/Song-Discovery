29754d7af70642e161f4f52edbbf9842a0fb2715
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2021-10-12 13:09:07 +0530

    small tweaks for deployment

--- main/app.py
+++ main/app.py
@@ -1,10 +1,18 @@
-from datetime import timedelta
+# from datetime import timedelta
 from flask import Flask
-
+from os import getenv
 
 # setting up the Flask app
 app = Flask(__name__)
-app.secret_key = "test"
+
+# check for environment
+is_prod = getenv("IS_HEROKU", None)
+
+# if we are in a production environment:
+if is_prod:
+    app.secret_key = getenv("SECRET_KEY")
+else:
+    app.secret_key = "test"
 
 # importing blueprint(s)
 from routes import views
